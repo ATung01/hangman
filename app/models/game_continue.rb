43e05a9108162bcb868ccf7b484bf492f04bcd31
github username <github email address>
2017-06-19 19:51:24 -0400

    updating show, worked on switching between users

--- app/models/game_continue.rb
+++ app/models/game_continue.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 def new_game_welcome(old_user)
   # TODO: Or, press to see all users and their winning ways...
-  puts "Do you want to play again? Y/N?"
+  puts "Do you want to play again? Y/N? Or, type 'show' to see Leaderboard..."
   answer = gets.chomp
   if answer.downcase == "y"
     new_game = Game.new(old_user.name)
@@ -11,6 +11,17 @@ def new_game_welcome(old_user)
     a = random_word_to_array(random_word)
     b = current_board(random_word)
     checker(a,b, old_user)
+  elsif answer == 'show'
+    # TODO: still a little wonky with the users...
+    User.all.collect do |user|
+      puts "#{user.name}:  wins: #{user.wins} losses: #{user.losses}"
+    end
+
+    # Paul:  wins: 2 losses: 0
+    # Paul:  wins: 0 losses: 0   <-- see, this is not good...
+    # Luke:  wins: 1 losses: 0
+    # Stacy:  wins: 0 losses: 1
+
   else
     puts "New player enter your name OR type EXIT to leave"
     answer = gets.chomp
